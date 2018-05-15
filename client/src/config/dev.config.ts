ac4b6620199a5bd56eb76ee014c846ece1ad3b96
IgorBabkin <igba14@gmail.com>
2018-05-15 13:57:31 +0300

    Cleanup

--- client/src/config/dev.config.ts
+++ client/src/config/dev.config.ts
@@ -1,8 +1,8 @@
-import {IConfig} from "./config.interface";
+import {IConfig} from './config.interface';
 
 export const devConfig: IConfig = {
     CONNECTION_URL: 'http://localhost:3002',
     API: {
         products: '/api/products',
-    }
+    },
 };
--- client/src/config/prod.config.ts
+++ client/src/config/prod.config.ts
@@ -4,5 +4,5 @@ export const prodConfig: IConfig = {
     CONNECTION_URL: process.env.BASE_NAME ? process.env.BASE_NAME : '',
     API: {
         products: '/api/products.json',
-    }
+    },
 };
